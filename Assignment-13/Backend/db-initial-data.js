3131b2c510be68fa0b990116e0542c5067d412da
AmaryaGunjan <gunjan.amarya@heb.com>
2018-12-04 21:30:37 +0530

    Update Assignment 13

---
+++ Assignment-13/Backend/db-initial-data.js
@@ -0,0 +1,36 @@
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    port: '3306',
+});
+
+con.connect(function (err) {
+    if (err) throw err;
+    con.query("CREATE DATABASE IF NOT EXISTS `e-mart`", function (err, result) {
+        if (err) throw err;
+    });
+    con.end();
+    con = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'root',
+        port: '3306',
+        database: 'e-mart'
+    });
+    con.query("CREATE TABLE IF NOT EXISTS `users` ( `username` VARCHAR(50) NOT NULL, `password` VARCHAR(50) NOT NULL, PRIMARY KEY(`username`, `password`))", function (err, result) {
+        if (err) throw err;
+        var users = [
+            ['dummy', 'happy'],
+        ]
+        con.query("INSERT INTO `users`(username,password) VALUES ?", [users], function (err, result) {
+            if (err) throw err;
+            con.query("CREATE TABLE IF NOT EXISTS `orders` ( `id` INT UNSIGNED NOT NULL AUTO_INCREMENT , `username` VARCHAR(50) NOT NULL, `cart` JSON NOT NULL, `amount_spent` INT UNSIGNED NOT NULL, purchase_timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, `order_status` VARCHAR(20) NOT NULL, PRIMARY KEY(`id`),  CONSTRAINT `username` FOREIGN KEY(`username`) REFERENCES `users`(`username`))", function (err, result) {
+                if (err) throw err;
+                con.end();
+            });
+        });
+    });
+});
--- Assignment-13/Backend/dbconnection.js
+++ Assignment-13/Backend/dbconnection.js
@@ -1,8 +1,9 @@
 var mysql = require('mysql');
-var connection = mysql.createConnection({ 
-    host:'localhost',
-    user:'root',
-    password:'',
-    database:'e-mart'
+var connection = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    port: '3306',
+    database: 'e-mart'
 });
-module.exports=connection;
+module.exports = connection;
